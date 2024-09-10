ff6fa65bad27087ff29ffe6df029ad3c93bc5f1b
andreyaraujo-dev <andreyaraujo.dev@gmail.com>
2024-09-10 20:16:17 -0300

    feat: create get week summary route

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,61 +1,29 @@
+import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 import {
   type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { z } from 'zod'
-import { createGoal } from '../functions/create-goal'
-import { createGoalCompletion } from '../functions/create-goal-completion'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createCompletionRouter } from './routes/create-completion'
+import { createGoalRoute } from './routes/create-goal'
+import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async request => {
-    const { title, desiredWeeklyFrequency } = request.body
-
-    await createGoal({
-      title,
-      desiredWeeklyFrequency,
-    })
-  }
-)
-
-app.get('/pending-goals', async () => {
-  return getWeekPendingGoals()
-})
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async request => {
-    const { goalId } = request.body
-
-    await createGoalCompletion({
-      goalId,
-    })
-  }
-)
+app.register(getWeekPendingGoalsRoute)
+app.register(createGoalRoute)
+app.register(createCompletionRouter)
+app.register(getWeekSummaryRoute)
 
 app.listen({ port: 3000 }).then(() => {
-  console.log('Fastify server listening on port 3000')
+  console.log('HTTP server listening on port 3000')
 })
