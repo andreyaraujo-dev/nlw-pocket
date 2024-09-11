e67b74f10ad7fbd2d3f35dc6bab5747081c085ef
andreyaraujo-dev <andreyaraujo.dev@gmail.com>
2024-09-11 00:04:44 -0300

    refactor: change component file name

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,5 @@
-import { Summary } from './components/Summary.tsx'
 import { CreateGoal } from './components/create-goal.tsx'
+import { Summary } from './components/summary.tsx'
 import { Dialog } from './components/ui/dialog.tsx'
 
 export function App() {
