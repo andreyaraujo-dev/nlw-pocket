de97e624fde6323fe108278e627e0e2b857f2adc
andreyaraujo-dev <andreyaraujo.dev@gmail.com>
2024-09-20 16:56:16 -0300

    feat: adds button to view goals

--- web/src/components/peding-goals.tsx
+++ web/src/components/peding-goals.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@/components/ui/button.tsx'
+import { Input } from '@/components/ui/input.tsx'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet.tsx'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { Plus } from 'lucide-react'
+import { Plus, Save, Trash } from 'lucide-react'
+import { useState } from 'react'
 import { createGoalCompletion } from '../actions/create-goal-completion.ts'
 import { getPendingGoals } from '../actions/get-pending-goals.ts'
 import { OutlineButton } from './ui/outline-button.tsx'
@@ -11,6 +15,9 @@ export function PendingGoals() {
     queryFn: getPendingGoals,
     staleTime: 1000 * 60, // 60 seconds
   })
+  const [goals, setGoals] = useState(data)
+
+  function handleChangeGoal() {}
 
   if (!data) return null
 
@@ -22,17 +29,49 @@ export function PendingGoals() {
   }
 
   return (
-    <div className="flex gap-3 flex-wrap">
-      {data.map(goal => (
-        <OutlineButton
-          key={goal.id}
-          disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
-          onClick={() => handleCompletionGoal(goal.id)}
-        >
-          <Plus className="size-4 text-zinc-600" />
-          {goal.title}
-        </OutlineButton>
-      ))}
-    </div>
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button type="button" variant="secondary">
+            Visualizar metas
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent className="flex flex-col gap-3 bg-zinc-950 border-zinc-900 pt-10">
+          {goals
+            ? goals.map(goal => (
+                <div key={goal.id} className="flex gap-2">
+                  <Input name={`title-${goal.id}`} value={goal.title} />
+                  <Input
+                    name={`desiredWeeklyFrequency-${goal.id}`}
+                    type="number"
+                    value={goal.desiredWeeklyFrequency}
+                    className="w-1/3"
+                  />
+
+                  <Button type="button">
+                    <Save size={15} />
+                  </Button>
+                  <Button type="button" variant="secondary">
+                    <Trash size={15} />
+                  </Button>
+                </div>
+              ))
+            : null}
+        </SheetContent>
+      </Sheet>
+      <div className="flex gap-3 flex-wrap">
+        {data.map(goal => (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompletionGoal(goal.id)}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        ))}
+      </div>
+    </>
   )
 }
